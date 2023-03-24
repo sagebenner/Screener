fdec8ef7279d9954c3fd94f241f061a515354555
sagebenner <sage.benner@gmail.com>
2023-03-24 13:53:20 -0500

    Updates SQL script to add studies and participant tables

--- screener script.sql
+++ screener script.sql
@@ -2,6 +2,8 @@ CREATE DATABASE dsq_screener;
 
 USE dsq_screener;
 
+select * from login;
+
 # This only stores data from the first 5 questions
 CREATE TABLE screen (
 id INT NOT NULL AUTO_INCREMENT,
@@ -54,4 +56,23 @@ email VARCHAR(255),
 password_hash VARCHAR(255),
 study_id INT,
 PRIMARY KEY (id)
+);
+
+CREATE TABLE studies (
+id INT NOT NULL AUTO_INCREMENT,
+study_name VARCHAR(255),
+researcher_id INT,
+study_access VARCHAR(255),
+PRIMARY KEY (id),
+FOREIGN KEY (researcher_id) REFERENCES researcher(id)
+);
+
+CREATE TABLE participant_list (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255),
+firstname VARCHAR(255),
+lastname VARCHAR(255),
+data_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (data_id) REFERENCES screen(id)
 );
