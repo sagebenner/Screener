42c64db13080c2f7899d8b5406dd117c1f7cb110
sagebenner <sage.benner@gmail.com>
2023-03-24 14:57:54 -0500

    Adds basic registration logic to the register function

--- website/login.py
+++ website/login.py
@@ -9,13 +9,15 @@ from flask_mysqldb import MySQL
 import bcrypt
 
 from website import mysql
+
 login = Blueprint('login', __name__)
 
+salt = bcrypt.gensalt()
 
 
 @login.route('/', methods=['post', 'get'])
 def start():
-    if request.method=='POST':
+    if request.method == 'POST':
         user_option = request.form.get('survey')
         session['user_option'] = user_option
         if user_option is not None:
@@ -25,20 +27,35 @@ def start():
                 return redirect(url_for('login.research'))
     return render_template('start.html')
 
+
 @login.route('/research', methods=['post', 'get'])
 def research():
-    if request.method=='POST':
-        if request.form['result']=='back':
+    if request.method == 'POST':
+        if request.form['result'] == 'back':
             return redirect(url_for('login.start'))
         else:
             return redirect(url_for('research_views.dashboard'))
     return render_template('research.html')
 
+
 @login.route('/register', methods=['post', 'get'])
 def register():
-    if request.method=='POST':
-        if request.form['result']=='back':
-            return redirect(url_for('login.start'))
+    if request.method == 'POST':
+        if request.form['result'] == 'back':
+            return redirect(url_for('login.research'))
+        else:
+            if request.form.get('firstname') is not None and request.form.get('lastname') is not None and \
+                    request.form.get('email') is not None and request.form.get('password') is not None:
+                firstname = request.form.get('firstname')
+                lastname = request.form.get('lastname')
+                email = request.form.get('email')
+                if bcrypt.hashpw(request.form.get('password').encode('utf-8'), salt) == \
+                        bcrypt.hashpw(request.form.get('password2').encode('utf-8'), salt):
+                    password_hash = bcrypt.hashpw(request.form.get('password').encode('utf-8'), salt)
+                    print("test:", password_hash)
+                    return redirect(url_for('login.research'))
+            else:
+                print('Not filled out')
     return render_template('register.html')
 
 
@@ -64,7 +81,7 @@ def log_in():
 
                 cursor.execute('SELECT id FROM login WHERE email = %s', (email,))
                 row = cursor.fetchone()
-                print("row",row)
+                print("row", row)
 
                 if row:
                     print(row[0])
@@ -82,18 +99,19 @@ def log_in():
                 error = "Please fill out all login information or click Continue as Guest"
                 session['user'] = "guest"
         if request.form['result'] == "guest":
-                session.clear()
-                session["checkbox"] = request.form.get("checkbox")
-                session['user'] = 'guest'
-                session.pop('logged_in', None)
-                session['logged_in'] = False
-                return redirect(url_for('screener_views.home'))
+            session.clear()
+            session["checkbox"] = request.form.get("checkbox")
+            session['user'] = 'guest'
+            session.pop('logged_in', None)
+            session['logged_in'] = False
+            return redirect(url_for('screener_views.home'))
     return render_template('login.html', error=error, mesg=mesg, cont=cont)
+
 
 @login.route('/consent', methods=['post', 'get'])
 def consent():
     consent_check = request.form.get('consent_check')
-    if request.method=='POST':
+    if request.method == 'POST':
         if consent_check == 'data':
             return redirect(url_for('screener_views.home'))
         else:
