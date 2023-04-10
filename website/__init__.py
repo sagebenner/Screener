3558a760b7a020236a8d100ef0565f85d57e01a6
sagebenner <sage.benner@gmail.com>
2023-04-10 10:02:38 -0500

    Changes default url to screener homepage

--- website/__init__.py
+++ website/__init__.py
@@ -19,8 +19,8 @@ def create_app():
     from .login import login
     from .research_views import research_views
     from .short_form import short_form
-    app.register_blueprint(login, url_prefix='/')
-    app.register_blueprint(screener_views, url_prefix='/screener/')
+    app.register_blueprint(login, url_prefix='/registration')
+    app.register_blueprint(screener_views, url_prefix='/')
     app.register_blueprint(research_views, url_prefix='/research/')
     app.register_blueprint(short_form, url_prefix='/short_form/')
 
